86d3405d93696dcc8f0f8f3c841a253c0dae8af4
iptsusho-tokuhin <87468971+iptsusho-tokuhin@users.noreply.github.com>
2023-02-06 10:48:33 +0900

    Update gas_javaf.js

--- js/gas_javaf.js
+++ js/gas_javaf.js
@@ -116,7 +116,7 @@ function output_to_html(input)
 	}
 
 	document.getElementById('index').textContent = [2][0][0];
-	document.getElementById('startes').innerHTML = "読込完了２";
+	document.getElementById('startes').innerHTML = "読込完了";
 	ButtonTypeChange();
 	Reach_Rate();
 	Set_Initial();
